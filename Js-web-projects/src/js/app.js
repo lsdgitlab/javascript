cb61ed9fa9761d2a07eeeafb295c60be62ae31c6
lasdgitlab <ravi8555@gmail.com>
2022-12-22 12:33:06 +0530

    newwebppackstrucure

---
+++ Js-web-projects/src/js/app.js
@@ -0,0 +1,136 @@
+import $ from "jquery";
+import 'normalize.css';
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel';
+import  '../css/sass/index.scss';
+import lozad from 'lozad';
+
+
+// module for lazy loading of images and video , etc.
+const observer = lozad();
+observer.observe();
+
+// import style1 from '../css/main.css';
+
+// animated-navigation.html
+import animatedN from "../js/animated-navigation.js";
+
+// // background-slider
+import moviApp from "../js/background-slider.js"; 
+
+import balloongame from "../js/balloon-game.js"; 
+
+
+
+
+// import component from './component.js';
+// import componentCircle from './circle.js'
+// 
+// import Nay from "../js/Nay.js";
+
+// // Edited
+// import editcode from "../js/edited.js";
+
+// codewars
+// import codewars from "../js/codewars.js";
+
+// // Leetcode
+// import leetCode from "../js/leetcode.js";
+
+// // dad-jokes
+// import DadJoke from "../js/dad-jokes.js";
+
+// event-keycodes.js
+// import ekeycode from "../js/event-keycodes.js";
+
+// faq-collapse
+// import faqCollapse from "../js/faq-collapse.js";
+
+// form
+import form from "../js/form.js"; 
+
+// // Progress 
+// import progrssStep from "../js/progress-steps.js";
+
+// Sound Board 
+// import soundBrd from "../js/sound-board.js";
+
+// incrementing-counter 
+// import increMent from "../js/incrementing-counter.js";
+
+// // Movie App
+// import moviApp from "../js/movie-app.js";
+
+// //random-choice-picker
+// import randChoice from "../js/random-choice-picker.js";
+
+
+
+// drinkwater
+// import animatedN from "../js/drink-water.js";
+
+
+
+// // button-ripple-effect
+// import btnRipplw from "../js/button-ripple-effect.js"; 
+
+// drawing-app
+// import draapp from "../js/drawing-app.js";
+
+// content-placeholder
+// import cntloadAnimt from "../js/content-placeholder.js";
+
+// drag-n-drop
+// import cntloadAnimt from "../js/drag-n-drop.js";
+
+// sticky-navigation
+// import sticky from "../js/sticky-navigation.js";
+
+// double-vertical-slider
+// import sticky from "../js/double-vertical-slider.js";
+
+// toast-notification
+// import sticky from "../js/toast-notification.js";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const heading = "Welcome to Digiserve";
+// document.querySelector('h2').innerText = heading 
+
+// console.log("check App JS");
+// cube
+
+// // leetcode
+// document.body.append(leetCode())
+
+// leetcode
+// document.body.append(form())
+
+// circle
+
+
+
+
+
+
+
+
+
+// get js file from the below file to use
+// import "./page-animation/barba";
--- Js-web-projects/webpack.config.js
+++ Js-web-projects/webpack.config.js
@@ -1,229 +1,202 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackPartialsPlugin = require('html-webpack-partials-plugin');
-// const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-// const TerserPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
 const webpack = require("webpack");
-const htmlPageNames = [
-  {
-      pageName: 'index.html',
-      title: ''
-  }, 
-  // {
-  //     pageName: 'architecture.html',
-  //     title: 'Lsdigital gallery'
-  // }, 
-  // {
-  //     pageName: 'detail-page.html',
-  //     title: 'Lsdigital gallery'
-  // }, 
-];
-let htmlFileName = htmlPageNames.map(htmlPage=> htmlPage.pageName);
-let multipleHtmlPlugins = htmlPageNames.map(name => {
-  return new HtmlWebpackPlugin({
-      // template: `./src/${name.pageName}`,
-      filename: `${name.pageName}`, 
-      // chunks: [`${name.pageName}`],
-      // chunks: [name.pageName.replace(/-(\w)/g, (match, c) => c.toUpperCase())],
-      title: name.title,
-      template: path.resolve(__dirname, `src/${name.pageName}`),
-  })
-});
+const devMode = process.env.NODE_ENV !== "production";
 
-module.exports = (env, argv) => ({
-  mode: argv.mode,
-  devtool: argv.mode === 'development' ? 'source-map' : false,
-  entry: [
-    './src/assets/js/app.js',
-    './src/assets/css/main.scss',
-  ],
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'js/[name].[contenthash].js',
-  },
-  optimization: {
-    minimizer: [
-      // new TerserPlugin(),
-      new OptimizeCSSAssetsPlugin(),
-      new CopyPlugin({
-        patterns: [
-          {from: 'src/*.html', to: '[name].[ext]'},
-          // { from: './src/assets', to: './assets' },
-          { from: "./src/assets/img", to: "./img" },
-          { from: "./src/assets/video", to: "./video" },
-          // { from: "./src/assets/favicon.ico", to: "./favicon.ico" },
-          // { from: './src/assets/images/*',to: 'images/[name].[ext]'}
-         ],
-      }),
-    ],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-            only: ["./src"],
-            // name: '[name][ext]',
-            // outputPath :'./js/'
-          }
-        },
-      }, {
-        test: /\.(scss)$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-          }, {
-            loader: 'css-loader',
-            options: {
-              url: false,
-            },
-          }, {
-            loader: 'postcss-loader',
-            options: {
-              postcssOptions: {
-                plugins: [
-                  ['autoprefixer'],
-                ],
-              },
-            },
-          }, {
-            loader: 'sass-loader',
-            options: {
-              implementation: require('sass'),
-            },
-          },
-        ],
-      },
-    ],
-  },
-  plugins: [
-    // new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin({
-      // filename: 'assets/app.css',
-      filename: argv.mode ? "./css/[name].[contenthash].css" : "./css/scss/[name].[contenthash].css",
-    }),
-    // new HtmlWebpackPartialsPlugin({
-    //   path:path.join(__dirname,'./src/toast-notification.html'),
-    //     location:'toastNotification',
-    //   template_filename: ['index.html', 'form.html']
-    // }),
-    //  new HtmlWebpackPartialsPlugin({
-    //     path:path.join(__dirname,'src/dad-jokes.html'),
-    //     location:'dadJoke',
-    //     template_filename: htmlFileName
-    //    }),
-    //     new HtmlWebpackPartialsPlugin({
-    //         path:path.join(__dirname,'src/event-keycodes.html'),
-    //         location:'eventkeycode',
-    //         template_filename: htmlFileName
-    //     }),
-    //    new HtmlWebpackPartialsPlugin({
-    //     path:path.join(__dirname,'src/faq-collapse.html'),
-    //     location:'faq',
-    //     template_filename: htmlFileName
-    //    }),
-       new HtmlWebpackPartialsPlugin({
-        path:path.join(__dirname,'./src/form.html'),
-        location:'form-validation',
-        // template: path.resolve(__dirname, 'src/index.html')
-        template_filename: htmlFileName
-       }),
-    //    new HtmlWebpackPartialsPlugin({
-    //     path:path.join(__dirname,'src/progress-steps.html'),
-    //     location:'progress-steps',
-    //     template_filename: htmlFileName
-    //    }),
-    //   new HtmlWebpackPartialsPlugin({
-    //     path:path.join(__dirname,'src/sound-board.html'),
-    //     location:'sound-board',
-    //     template_filename: htmlFileName
-    //    }),
-    //   new HtmlWebpackPartialsPlugin({
-    //     path:path.join(__dirname,'src/incrementing-counter.html'),
-    //     location:'counter',
-    //     template_filename: htmlFileName
-    //    }),
-    //    new HtmlWebpackPartialsPlugin({
-    //     path:path.join(__dirname,'src/movie-app.html'),
-    //     location:'movie',
-    //     template_filename: htmlFileName
-    //    }),
-    // new HtmlWebpackPartialsPlugin({
-    //     path:path.join(__dirname,'src/random-choice-picker.html'),
-    //     location:'random-color',
-    //     template_filename: htmlFileName
-    //    }),
-    // new HtmlWebpackPartialsPlugin({
-    //     path:path.join(__dirname,'src/animated-navigation.html'),
-    //     location:'navigation',
-    //     template_filename: htmlFileName
-    //    }),
-    
-    //    new HtmlWebpackPartialsPlugin({
-    //     path:path.join(__dirname,'src/drink-water.html'),
-    //     location:'drinkwater',
-    //     template_filename: htmlFileName
-    //    }),
-    //    new HtmlWebpackPartialsPlugin({
-    //     path:path.join(__dirname,'src/background-slider.html'),
-    //     location:'background',
-    //     template_filename: htmlFileName
-    //    }),
-    //    new HtmlWebpackPartialsPlugin({
-    //     path:path.join(__dirname,'src/button-ripple-effect.html'),
-    //     location:'ripple',
-    //     template_filename: htmlFileName
-    //    }),
-    //    new HtmlWebpackPartialsPlugin({
-    //     path:path.join(__dirname,'src/drawing-app.html'),
-    //     location:'drwaapp',
-    //     template_filename: htmlFileName
-    //    }),
-    //     new HtmlWebpackPartialsPlugin({
-    //     path:path.join(__dirname,'src/content-placeholder.html'),
-    //     location:'contPlace',
-    //     template_filename: htmlFileName
-    //    }),
-    //    new HtmlWebpackPartialsPlugin({
-    //     path:path.join(__dirname,'src/drag-n-drop.html'),
-    //     location:'drag-and-drop',
-    //     template_filename: htmlFileName
-    //    }),
-    //     new HtmlWebpackPartialsPlugin({
-    //     path:path.join(__dirname,'src/sticky-navigation.html'),
-    //     location:'sticky-navbar',
-    //     template_filename: htmlFileName
-    //    }),
-    //    new HtmlWebpackPartialsPlugin({
-    //     path:path.join(__dirname,'src/double-vertical-slider.html'),
-    //     location:'doublevsld',
-    //     template_filename: htmlFileName
-    //    }),
-    // new HtmlWebpackPartialsPlugin({
-    //     path:path.join(__dirname,'./src/footer.html'),
-    //     location:'footer',
-    //     template_filename: htmlFileName
-    // }),
-    new webpack.ProvidePlugin({
-      $: "jquery",
-      jQuery: "jquery",
-      'window.jQuery': 'jquery'
-    }),
-  ].concat(multipleHtmlPlugins),
-  devServer: {
-    contentBase: [
-      path.join(__dirname, '/src'),
-    ],
-    watchContentBase: true,
-    // compress: true,
-    port: 9000,
-    open: true,
-  },
+// all pages name that require in the project
+const htmlPageNames = [
+    {
+        pageName: 'index.html',
+        title: 'Home'
+    },
+    {
+        pageName:'animated-navigation.html',
+        title: 'Animated Navigation'
+    }, 
+    {
+        pageName: 'background-slider.html',
+        title: ''
+    }, 
+    {
+        pageName: 'balloon-game.html',
+        title: 'Balloon game'
+    }, 
+    {
+        pageName: '.html',
+        title: ''
+    }, 
+    {
+        pageName: '.html',
+        title: ''
+    }, 
+    {
+        pageName: '.html',
+        title: ''
+    }, 
+    {
+        pageName: '.html',
+        title: ''
+    }, 
+    {
+        pageName: '.html',
+        title: ''
+    }, 
+    // {
+    //     pageName: '.html',
+    //     title: ''
+    // }, 
+    // {
+    //     pageName: '.html',
+    //     title: ''
+    // }, 
+    // {
+    //     pageName: '.html',
+    //     title: ''
+    // }, 
+    // {
+    //     pageName: '.html',
+    //     title: ''
+    // }, 
+    // {
+    //     pageName: '.html',
+    //     title: ''
+    // }, 
+    // {
+    //     pageName: '.html',
+    //     title: ''
+    // }, 
+    // {
+    //     pageName: '.html',
+    //     title: ''
+    // }, 
+    // {
+    //     pageName: '.html',
+    //     title: ''
+    // }, 
+    // {
+    //     pageName: '.html',
+    //     title: ''
+    // }, 
+    // {
+    //     pageName: '.html',
+    //     title: ''
+    // }, 
+    // {
+    //     pageName: '.html',
+    //     title: ''
+    // }, 
+    // {
+    //     pageName: '.html',
+    //     title: ''
+    // }, 
+    // { 
+    //     pageName: '.html',
+    //     title: ''
+    // }
+];
+// get the html page name
+let htmlFileName = htmlPageNames.map(htmlPage=> htmlPage.pageName);
+
+// convert the page into array to concatinate in pulgin
+let multipleHtmlPlugins = htmlPageNames.map(name => {
+    return new HtmlWebpackPlugin({
+        filename: `${name.pageName}`, 
+        title: name.title,
+        template: path.resolve(__dirname, `src/${name.pageName}`),
+    })
 });
+module.exports = (env, argv) => ({
+    entry:{
+        // entery for main file
+        main:path.resolve(__dirname, 'src/js/app.js'),
+    },
+    output:{
+        // output for dist folder
+        path: path.resolve(__dirname, 'dist'),
+        filename:'./js/[name].[contenthash].js',
+        clean:true,
+    },
+    // if source map of js file 
+    devtool: 'source-map',
+    // devserver is use to run html pages on browser we can also set the port
+    devServer:{
+        static: path.resolve(__dirname, 'src'),
+        port: 8000,
+        open: true,
+        hot: true,
+    },
+    // loaders
+    module:{
+        rules:[
+            //css 
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [MiniCssExtractPlugin.loader,"css-loader","sass-loader"], 
+            },
+            //images 
+            {
+                test: /\.(svg|png|jpg|webp|ico|jpeg)$/,
+                type: 'asset/resource',
+                
+            },
+            // js babel for convert ts file to js file
+            {   
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                        only: ["./src"],
+                        // name: '[name][ext]',
+                        // outputPath :'./js/'
+                    }
+                }
+
+            }
+            
+        ]
+    },
+   
+    //  // Plugins
+    plugins:[
+        // this is use for path of the file for css path in dev and prod
+        new MiniCssExtractPlugin({
+            filename: devMode ? "./css/scss/[name].[contenthash].css" : "[name].css",
+        }),
+
+        // HtmlWebpackPartialsPlugin is use for intgrate componet in html pages  
+        // new HtmlWebpackPartialsPlugin({
+        //     path:path.join(__dirname,'./src/header.html'),
+        //     location:'header',
+        //     template_filename: htmlFileName
+        // }),
+
+        // HtmlWebpackPartialsPlugin is use for intgrate componet in html pages  
+        // new HtmlWebpackPartialsPlugin({
+        //     path:path.join(__dirname,'./src/footer.html'),
+        //     location:'footer',
+        //     template_filename: htmlFileName
+        // }),
+        // to use jquery globle in project 
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery",
+            'window.jQuery': 'jquery'
+        }),
+       // copy asset of dist in prod
+        new CopyPlugin({
+            patterns: [
+                { from: './src/assets', to: './assets' },
+                { from: "./src/img", to: "./img" },
+                { from: "./src/video", to: "./video" },
+                // { from: "./src/favicon.ico", to: "./favicon.ico" },
+            ],
+          })
+      
+    //    add  the html pages in plugin
+    ].concat(multipleHtmlPlugins)
+})
